7c5c4e45f6884f9ef082ea1bdd34182dc7eb09df
Nick Story <storynickolasl@gmail.com>
2022-10-19 21:01:39 -0400

    Add Routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/1" do
+    Workout.first.exercises.to_json
+  end
+
+  get "/2" do
+    Workout.second.exercises.to_json
+  end
+
+  get "/3" do
+    Workout.third.exercises.to_json
   end
 
 end
